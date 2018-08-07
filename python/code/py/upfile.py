6ed78a2680e1c4967fd733dc05be25d51db1e20f
niezongxia <41997833+niezongxia@users.noreply.github.com>
2018-08-07 16:42:31 +0800

    upfile增加循环上传
    增加循环，使目录下所有文件都能上传

--- python/code/py/upfile.py
+++ python/code/py/upfile.py
@@ -9,6 +9,7 @@ def ftp_open(host,user,pwd):
     ftp.login(user,pwd)
     print(ftp.getwelcome())
     ftp.cwd('/ydweb/war')
+    bufsize=-1
     return ftp
 def ftp_up(ftp,filename):
     file_handler=open(filename,'rb')
@@ -18,6 +19,17 @@ def ftp_up(ftp,filename):
 def ftp_close(ftp):
     ftp.set_debuglevel(0)
     ftp.quit()
+def file_ls(path):
+    ls=os.listdir(path)
+    for i in ls:
+        cur_path=os.path.join(path,i)
+        if os.path.isdir(cur_path):
+            file_ls(cur_path)
+        else:
+            c_path=cur_path.replace("\\","/")
+            print(c_path)
+            ftp_up(ftp,c_path)
+            os.remove(c_path)
 ftp=ftp_open(sys.argv[1],sys.argv[2],sys.argv[2])
-ftp_up(ftp,sys.argv[3])
+file_ls(sys.argv[3])
 ftp_close(ftp)
